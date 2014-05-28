1c1116b0a568b0b8e043b82651e1e9b026fd2ff8
Aslak Knutsen <aslak@4fs.no>
2014-05-28 18:29:25 +0200

    Re org project to multiple modules

---
+++ mods/org.cbx~cbx-backend~1.0/neo.js
@@ -0,0 +1,9 @@
+load('./npm_modules.js');
+
+var neo4j = require('neo4j');
+
+//var db = new neo4j.GraphDatabase('http://localhost:7474');
+
+function vertxStop() {
+   console.log('Verticle has been undeployed');
+}
--- mods/org.cbx~cbx~1.0/app.rb
+++ mods/org.cbx~cbx~1.0/app.rb
@@ -1,17 +1,20 @@
 require "vertx"
 
-
 appConfig = {
     :frontend => {
         # Config for verticle1
     },
     :lanyrd => {
         # Config for verticle2
+    },
+    :neo => {
+        # Config for verticle2
     }
 }
 
 puts "Starting backend.."
-Vertx.deploy_worker_verticle('lanyrd_handler.rb', appConfig[:lanyrd]) {
+Vertx.deploy_module('org.cbx~cbx-backend~1.0', appConfig[:neo]);
+Vertx.deploy_verticle('lanyrd_handler.rb', appConfig[:lanyrd]) {
     puts "Starting frontend.."
     Vertx.deploy_verticle('server.rb', appConfig[:frontend])
 }
